67c1489af0b76409c56c943dbba08d743f92bb74
MikhailPakaliuk <mr.dyurilen@mail.ru>
2020-02-20 17:27:47 +0300

    feat: Task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array=0) {
+  return array==0 ? 0 : array.sort(compareSort)[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array=0) {
+  return array==0 ? 0 : array.sort(compareSort).pop();
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array=0) {
+  return array==0 ? 0 : array.reduce((a, b) => a + b) / array.length;
 }
+
+const compareSort = (a, b) => a - b;
